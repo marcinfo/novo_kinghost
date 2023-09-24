eb04bf970b9594044e780362bed27c289ef8b4dc
marcinfo <90268921+marcinfo@users.noreply.github.com>
2023-09-24 11:44:36 -0300

    king host

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ if SECRET_KEY is None:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['www.pragas.kinghost.net']
 X_FRAME_OPTIONS = '*'
 
 # Application definition
